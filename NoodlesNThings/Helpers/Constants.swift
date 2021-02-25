e1cabf7627aeca3d3bb81551434e15330f505f48
Joe V <joeveverka89@gmail.com>
2021-02-25 09:36:02 -0500

    Custom Alert Controller. Constants.

---
+++ NoodlesNThings/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NoodlesNThings
+//
+//  Created by Joseph Veverka on 2/25/21.
+//
+
+import UIKit
+
+class Constant: NSObject {
+    static let kAppTitle = "Noodles 'n Things"
+    static let kUserDefault = UserDefaults.standard
+    static let kIsUserRegistered = "isRegister"
+}
---
+++ NoodlesNThings/Helpers/MessageConstant.swift
@@ -0,0 +1,14 @@
+//
+//  MessageConstant.swift
+//  NoodlesNThings
+//
+//  Created by Joseph Veverka on 2/25/21.
+//
+
+import Foundation
+
+class Message {
+    static let kNoNetwork = "Please check your internet connection."
+    static let kBlankEmail = "Please enter Email."
+    static let kBlankPwd = "Please enter Password."
+}
